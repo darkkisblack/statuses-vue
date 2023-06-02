7db66cfedbb6fa06683a0029e22e4f278ec59f20
Roman <darkkisblack1994@yandex.ru>
2023-06-02 21:09:04 +0700

    done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,82 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <status-column
+      v-for="status in sortedStatuses"
+      :key="status.STATUS_ID"
+      :statusName="status.TITLE"
+      :deals="status.deals"
+    ></status-column>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import axios from 'axios';
+import StatusColumn from './components/StatusColumn.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld,
+    StatusColumn,
+  },
+  data() {
+    return {
+      statuses: [],
+      deals: [],
+    };
+  },
+  computed: {
+    sortedStatuses() {
+      return [...this.statuses].sort((a, b) => a.SORT - b.SORT);
+    },
+  },
+  mounted() {
+    this.fetchStatuses();
+  },
+  methods: {
+    async fetchStatuses() {
+      try {
+        const response = await axios.get(
+          'https://nastintesthodl.stocrm.ru/api/external/v1/offer/all_statuses?SID=10813_0c0a9a2f86eab09196705a274378b64a&BOARD_ID=1843',
+        );
+        const responseData = response.data.RESPONSE;
+        this.statuses = Object.entries(responseData).map(([, value]) => ({ ...value }));
+
+        await this.fetchDeals();
+      } catch (error) {
+        this.handleError(error);
+      }
+    },
+    async fetchDeals() {
+      try {
+        const response = await axios.get(
+          'https://nastintesthodl.stocrm.ru/api/external/v1/offers/get_from_filter?SID=10813_0c0a9a2f86eab09196705a274378b64a&PAGE=1&LIMIT=9999&REQUIRED_FIELDS[0]=',
+        );
+        const responseData = response.data.RESPONSE.DATA;
+        this.deals = Array.from(responseData);
+        this.mapDealsToStatuses();
+      } catch (error) {
+        this.handleError(error);
+      }
+    },
+    mapDealsToStatuses() {
+      this.statuses = this.statuses.map((status) => {
+        const { STATUS_ID } = status;
+        return {
+          ...status,
+          deals: this.deals.filter((deal) => deal.OFFER_STATUS_ID === STATUS_ID),
+        };
+      });
+    },
+    handleError(error) {
+      console.error(error);
+    },
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.app {
+  display: flex;
+  gap: 20px;
+  padding: 20px;
 }
 </style>
---
+++ src/components/StatusColumn.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="status-column">
+    <h2>{{ statusName }}</h2>
+    <div class="deal-cards">
+      <div v-for="deal in deals" :key="deal.OFFER_ID" class="deal-card">
+        <p>{{ deal.OFFER_FIRST_RESPONSIBLE }}</p>
+        <p>#{{ deal.OFFER_ID }}</p>
+        <p>{{ deal.OFFERS_TYPE_NAME || '-' }}</p>
+        <p>{{ deal.SEGMENT_NAME }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    statusName: String,
+    deals: Array,
+  },
+};
+</script>
+
+<style scoped>
+.status-column {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.deal-cards {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.deal-card {
+  background-color: #f0f0f0;
+  padding: 10px;
+}
+</style>
